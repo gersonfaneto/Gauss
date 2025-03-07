0b0504de76f73d795b670e657f8b1d8ba8ba6ceb
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-03-07 07:41:05 -0300

    build(nix): setup `flake`

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+            # stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              bear
+              valgrind
+              clang-tools
+              # cmake
+              # codespell
+              # conan
+              # cppcheck
+              # doxygen
+              # gtest
+              # lcov
+              # vcpkg
+              # vcpkg-tool
+              shfmt
+              bash-language-server
+              efm-langserver
+            ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+          };
+      });
+    };
+}
+
