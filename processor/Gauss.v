852ba072d457998d16eeba2867b8576f8862fdb6
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-03-23 23:48:21 -0300

    feat(processor): add `Counter` module

--- processor/Gauss.v
+++ processor/Gauss.v
@@ -1,5 +1,17 @@
 module Gauss (
-    x
+    clock,
+    reset,
+    counter
 );
-  input wire x;
+
+  input wire clock;
+  input wire reset;
+  output wire [3:0] counter;
+
+  Counter C0 (
+      .clock  (clock),
+      .reset  (reset),
+      .counter(counter)
+  );
+
 endmodule
