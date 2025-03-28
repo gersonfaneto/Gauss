2c35ddc44cfd8a95da90e029658ad0614932902e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-03-28 09:08:37 -0300

    feat(core): add `RAM` module

--- core/Gauss.v
+++ core/Gauss.v
@@ -1,3 +1,20 @@
+/*
+ * What's missing?
+ *
+ *   - `Buffer` :: Where the user data is going to be writen to and temporarily
+ *     stored.
+ *
+ *   - `Decoder` :: Gets the data held by the buffer and unwraps it.
+ *
+ *   - `Controller` :: Analyzes the received instruction/information and handles
+ *     it to the proper parts of the circuit.
+ *
+ *   - `ALU` :: Where the ¹main operations are performed.
+ *
+ * ¹ The smaller operations (e.g. like adding two numbers) may be implemented
+ * on a separate module.
+ */
+
 module Gauss (
     clock
 );
