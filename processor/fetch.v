b3476175e0b9e38901b793775fd4555808f364d8
marcosviniciussena <marcvinisena@gmail.com>
2025-04-09 14:27:34 -0300

    Fetch ok

--- processor/fetch.v
+++ processor/fetch.v
@@ -1,17 +1,17 @@
 module fetch(
     input clk,                
     input reset,              
-    input branch,             
-    input [15:0] branch_addr, 
+    input branch,              // sinal para indicar desvio 
+    input [15:0] branch_addr,  // endereço para desvio na instrução
     input instr_done,          // Indica que a instrução foi executada
-    output reg [15:0] instr,  
+    output reg [25:0] instr,  
     output reg [15:0] pc      
 );
 
-    reg [15:0] instruction_memory [0:255]; 
+    reg [25:0] instruction_memory [0:255]; 
     
     initial begin
-        $readmemh("instructions.hex", instruction_memory);
+        $readmemh("instructions.hex", instruction_memory);  // tem que colocar uma lista de instruções verdadeira
     end
 
     always @(posedge clk or posedge reset) begin
@@ -19,7 +19,7 @@ module fetch(
             pc <= 16'b0; 
         end else if (instr_done) begin // Só busca a próxima se a atual terminou
             instr <= instruction_memory[pc]; 
-            pc <= branch ? branch_addr : pc + 1; 
+            pc <= branch ? branch_addr : pc + 1; // se não houver uym desvio na instrução, segue o fluxo para pc+1 
         end
     end
 
